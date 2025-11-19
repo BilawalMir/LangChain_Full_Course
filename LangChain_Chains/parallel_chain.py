03994962d5edd955fc6c777171d925777a167078
Bilawal Mir <bilawalijaz2019@gmail.com>
2025-11-19 19:44:36 +0500

    update

--- LangChain_Chains/parallel_chain.py
+++ LangChain_Chains/parallel_chain.py
@@ -15,7 +15,7 @@ model2 = ChatAnthropic(model_name="claude-3-7-sonnet-20240922")
 
 prompt1 = PromptTemplate(
     template="Generate short and simple notes from the following  text \n {text}?",
-    input_variables=["text"]
+    input_variables=["text"],
 )
 
 prompt2 = PromptTemplate(
@@ -24,18 +24,15 @@ prompt2 = PromptTemplate(
 )
 
 prompt3 = PromptTemplate(
-  template="Merge the provided notes and quiz into a single document \n notes -> {notes} and quiz-> {quiz}"
-  input_variables=['notes', 'quiz']
+    template="Merge the provided notes and quiz into a single document \n notes -> {notes} and quiz-> {quiz}",
+    input_variables=["notes", "quiz"],
 )
 
 parser = StrOutputParser()
 
 parallel_chain = RunnableParallel(
-  {
-    "notes": prompt1 | model1 | parser,
-    "quiz": prompt2 | model2 | parser
-  }
- )
+    {"notes": prompt1 | model1 | parser, "quiz": prompt2 | model2 | parser}
+)
 
 merge_chain = prompt3 | model1 | parser
 
