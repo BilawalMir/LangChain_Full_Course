51fdff523cc290e8af059ba49b083f8fa316b60d
Bilawal Mir <bilawalijaz2019@gmail.com>
2025-11-21 18:26:13 +0500

    update

--- LangChain_Runnables/runnable_lambda.py
+++ LangChain_Runnables/runnable_lambda.py
@@ -4,7 +4,6 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import (
     RunnableParallel,
-    RunnableBranch,
     RunnableLambda,
     RunnableSequence,
     RunnablePassthrough,
@@ -12,32 +11,33 @@ from langchain_core.runnables import (
 
 load_dotenv()
 
+
 def word_count(text):
     return len(text.split())
+
 
 model = ChatOpenAI()
 
 parser = StrOutputParser()
 
 
-prompt = PromptTemplate(
-    template="Write a joke about {topic}", input_variable=["topic"]
+prompt = PromptTemplate(template="Write a joke about {topic}", input_variable=["topic"])
+
+joke_gen_chain = RunnableSequence(prompt, model, parser)
+
+parallel_chain = RunnableParallel(
+    {"joke": RunnablePassthrough(), "word_count": RunnableLambda(word_count)}
 )
 
-joke_gen_chain = RunnableSequence(prompt , model , parser)
+# OR
+parallel_chain1 = RunnableParallel(
+    {
+        "joke": RunnablePassthrough(),
+        "word_count": RunnableLambda(lambda x: len(x.split())),
+    }
+)
 
-parallel_chain = RunnableParallel({
-  'joke': RunnablePassthrough(),
-  'word_count': RunnableLambda(word_count )
-})
-
-#OR
-parallel_chain1 = RunnableParallel({
-  'joke': RunnablePassthrough(),
-  'word_count': RunnableLambda(lambda x: len(x.split()) )
-})
-
-final_chain = RunnableSequence(joke_gen_chain , parallel_chain)
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
 result = final_chain.invoke({"topic": "AI"})
-final_result = """{} \n word count - {}""".formate(result['joke'], result['word_count'])
+final_result = """{} \n word count - {}""".formate(result["joke"], result["word_count"])
 print(final_result)
