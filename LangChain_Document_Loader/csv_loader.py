70910f61f0630fd5d8df8fb6dd8c68a17b86673f
Bilawal Mir <bilawalijaz2019@gmail.com>
2025-11-23 18:50:30 +0500

    update

---
+++ LangChain_Document_Loader/csv_loader.py
@@ -0,0 +1,7 @@
+from langchain_community.document_loaders import CSVLoader
+
+loader = CSVLoader(file_path='D:/LangChain_Models/LangChain_Document_Loader/Social_Network_Ads.csv')
+
+docs = loader.load()
+print(len(docs))
+print(docs[0])
