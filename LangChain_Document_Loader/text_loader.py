8f368f4d79f085f5ca3400530db7528fe3372840
Bilawal Mir <bilawalijaz2019@gmail.com>
2025-11-23 18:02:04 +0500

    update

---
+++ LangChain_Document_Loader/text_loader.py
@@ -0,0 +1,31 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI()
+parser = StrOutputParser()
+
+prompt = PromptTemplate(
+    template= 'write the summary of the following poem - \n {poem}'
+    input_variables=['poem']
+)
+
+loader = TextLoader("D:\LangChain_Models\LangChain_Document_Loader\cricket.txt", encoding='utf-8')
+
+docs = loader.load()
+
+print(type(docs))
+
+print(len(docs))
+
+print(docs[0].page_content)
+
+print(docs[0].metadata)
+
+chain = prompt | model | parser
+
+chain.invoke({'poem': docs[0].page_content})
