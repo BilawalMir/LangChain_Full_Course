45f2618626e117e9fcde2dfc48701b4800598a92
Bilawal Mir <bilawalijaz2019@gmail.com>
2025-12-13 22:42:49 +0500

    added new file

--- LnagChain_Text_Splitters/length_based.py
+++ LnagChain_Text_Splitters/length_based.py
@@ -1,18 +1,16 @@
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 
-loader = PyPDFLoader(file_path='D:\LangChain_Models\LnagChain_Text_Splitters\dl-curriculum.pdf')
+loader = PyPDFLoader(
+    file_path="D:\LangChain_Models\LnagChain_Text_Splitters\dl-curriculum.pdf"
+)
 
 text = """Space exploration has led to incredible scientific discoveries. From landing on the Moon to exploring Mars, humanity continues to push the boundaries of what’s possible beyond our planet.
 
 These missions have not only expanded our knowledge of the universe but have also contributed to advancements in technology here on Earth. Satellite communications, GPS, and even certain medical imaging techniques trace their roots back to innovations driven by space programs.
 """
 docs = loader.load()
-splitter = CharacterTextSplitter(
-  chunk_size=100,
-  chunk_overlap=0,
-  separator=''
-)
+splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0, separator="")
 result = splitter.split_text(text)
 print(result)
 
