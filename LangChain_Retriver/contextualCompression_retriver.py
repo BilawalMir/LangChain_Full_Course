c435c32f00ac4b26307f23fc619dcc9a6244ce1a
Bilawal Mir <bilawalijaz2019@gmail.com>
2025-12-16 21:28:44 +0500

    update

--- LangChain_Retriver/contextualCompression_retriver.py
+++ LangChain_Retriver/contextualCompression_retriver.py
@@ -1,9 +1,9 @@
 from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAIEmbeddings 
+from langchain_openai import OpenAIEmbeddings
 from langchain_openai import ChatOpenAI
 from langchain_core.documents import Document
 from dotenv import load_dotenv
-from langchain.retrivers.contextual_compression  import ContextualCompressionRetriever
+from langchain.retrivers.contextual_compression import ContextualCompressionRetriever
 from langchain.retrievers.documents_compressors import LLMChainExtractor
 
 load_dotenv()
@@ -27,12 +27,12 @@ vector_store = FAISS.from_documents(
     embeddings=embeddings,
 )
 
-base_retriever = vector_store.as_retriever(search_kwargs = {"k": 5})
+base_retriever = vector_store.as_retriever(search_kwargs={"k": 5})
 
 llm = ChatOpenAI(model="gpt-3.5-turbo")
 compressor = LLMChainExtractor.from_llm(llm)
 
-#create contextual compressor retriever
+# create contextual compressor retriever
 compression_retriever = ContextualCompressionRetriever(
     base_retriever=base_retriever,
     compressor=compressor,
@@ -43,10 +43,6 @@ query = "what is photosynthesis?"
 compressed_results = compression_retriever.invoke(query)
 
 
-for i , doc in enumerate(compressed_results):
+for i, doc in enumerate(compressed_results):
     print(f"\n --- {i+1}---")
     print(doc.page_content)
-
-
-
- 
